71049f7aaab6791ef086a6bd9782aa10ce80e20a
Tazkia Tasnim Bahar <105160856+Tazkia189@users.noreply.github.com>
2022-05-11 08:49:55 -0700

    factory pattern implemented

--- src/Umbrella/MainClass.java
+++ src/Umbrella/MainClass.java
@@ -6,3 +6,39 @@ public class MainClass {
         HiddenClass hc=new HiddenClass("khoshnuma");
     }
 }
+class Circle implements MainClass{
+	 @Override
+	 public void draw()
+	 {
+		 System.out.println("a circle drawn");
+	 }
+}
+class Rectangle implements MainClass{
+	 @Override
+	 public void draw()
+	 {
+		 System.out.println("a rectangle drawn");
+	 }
+}
+class UmbrellaShapeFactory{ 
+	 public MainClass getShape(String type) throws Exception {
+		 switch (type) {
+			 case "Circle":
+				 return new Circle();
+			 case "Rectangle":
+				 return new Rectangle();
+			 default:
+				 throw new Exception( "Shape type : "+type+" cannot be instantiated");
+		 }
+	 }
+}
+class PaintUmbrella{
+	 public static void main(String[] args) throws Exception	 {
+		 
+		 UmbrellaShapeFactory shapeFactory = new UmbrellaShapeFactory();
+		 MainClass circle=shapeFactory.getShape("Circle");
+		 circle.draw();
+		 MainClass rectangle = shapeFactory.getShape("Square");
+		 square.draw();
+	 }
+}
